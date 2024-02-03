55c1188c810506e4be61293f735428853eb7e775
sanjay <sanjay.11801603@gmail.com>
2024-02-03 20:11:36 +0530

    Added the mobile optimization

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import logo from '../../../public/sanjay.svg'
 import Image from "next/image"
 import './Header.css'
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const handleManu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
     return (
         <div className='header'>
             <div className='flex justify-between items-center'>
@@ -28,11 +34,16 @@ function Header() {
                     <a href="/blogs">Blogs</a>
                     <a href="/exprience">Experience</a>
                 </div>
-                <div >
+                <div className='contact'>
                     <a href="/contact" className='flex justify-center items-center'>
                         <div className='dot me-3'></div>
                         <div className='contact-text'>Contact Me</div>
                     </a>
+                    <div className='action' onClick={handleManu}>
+                        <div className={`${isMenuOpen ? 'top-line-open' : 'top-line'} mb-[6px]`}></div>
+                        <div className={isMenuOpen? 'middle-line-open' : 'middle-line'}></div>
+                        <div className={`${ isMenuOpen ? 'bottom-line-open' : 'bottom-line'} mt-[6px]`}></div>
+                    </div>
                 </div>
             </div>
         </div>
